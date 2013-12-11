b60f80206dd661f7efd04eb03efca16d84f2b79a
HenrikDetjen@googlemail.com <HenrikDetjen@googlemail.com@36e90b45-c853-0ed4-9bd9-cd0fc73b72b9>
2013-12-11 11:16:29 +0000

    update gui

--- SocialMediaExplorer/src/main/java/ude/SocialMediaExplorer/client/gui/MainPage.java
+++ SocialMediaExplorer/src/main/java/ude/SocialMediaExplorer/client/gui/MainPage.java
@@ -1,7 +1,13 @@
 package ude.SocialMediaExplorer.client.gui;
 
+import java.util.ArrayList;
+
+import ude.SocialMediaExplorer.shared.exchangeFormat.ClusterElement;
+
+import com.github.gwtbootstrap.client.ui.ListBox;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -15,7 +21,18 @@ public class MainPage extends Composite {
 
 	public MainPage() {
 		initWidget(uiBinder.createAndBindUi(this));
+		
+		ArrayList<String> names = new ArrayList<String>();
+		names.add("tatort");
+		names.add("halligalli");
+		names.add("tvog");
+		names.add("sterntv");
+		names.add("berlintagundnacht");
+		for (String name : names) {
+			listSeries.addItem(name);
+		}
 	}
 	
-
+	@UiField ListBox listSeries;
+	
 }
